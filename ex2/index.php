50b64b426efec9e21b1f235f7aa2397c89137cbb
morgan1000 <morgan.gardes.19@gmail.com>
2026-05-22 15:21:33 +0200

    ex2 finit mais à corriger

--- ex2/index.php
+++ ex2/index.php
@@ -1,4 +1,8 @@
-
+<?php 
+// var_dump($_GET);
+$age = $_GET['age'];
+// $age = 100;
+?>
 
 
 <!DOCTYPE html>
@@ -9,6 +13,17 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <!-- <a href="index.php?lastname=Nemare&firstname=Jean&age=<?= $age ?>">Page</a> -->
+    <a href="index.php?lastname=Nemare&firstname=Jean">Page</a>
+    <?php 
+    if (isset($_GET['age']))
+    {
+        echo $age;
+    }
+    else {
+        echo "Le paramètre âge n'existe pas.";
+        return;
+    }
+    ?>
 </body>
 </html>
