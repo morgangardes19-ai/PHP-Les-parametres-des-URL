7b906ba449a7271d482a3d1606a5bc60fc137ab1
morgan1000 <morgan.gardes.19@gmail.com>
2026-05-22 15:34:30 +0200

    ex3 à corriger

--- ex3/index.php
+++ ex3/index.php
@@ -1,4 +1,8 @@
-
+<?php 
+var_dump($_GET);
+$startDate = $_GET['startDate'];
+$endDate = $_GET['endDate'];
+?>
 
 
 <!DOCTYPE html>
@@ -9,6 +13,8 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <a href="index.php?startDate=2/05/2016&endDate=27/11/2016">Page</a>
+    <p>Date de début : <?= $startDate ?></p>
+    <p>Date de fin : <?= $endDate ?></p>
 </body>
 </html>
